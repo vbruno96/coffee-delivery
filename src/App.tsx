a50e35346b346c09cd5650451e3982754d8cf687
Bruno Vinícius <vbruno96@gmail.com>
2023-05-16 18:35:24 -0300

    create delivery page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './global.css'
 import { Cart } from './pages/Cart'
+import { Delivery } from './pages/Delivery'
 import { Shopp } from './pages/Shopp'
 
 export function App() {
@@ -7,7 +8,7 @@ export function App() {
   return (
     <div className='min-h-screen bg-gray-50 antialiased'>
       <div className='w-full max-w-screen-2xl px-3 xl:mx-auto xl:px-40'>
-        <Cart />
+        <Delivery />
       </div>
     </div>
   )
---
+++ src/pages/Delivery/index.tsx
@@ -0,0 +1,52 @@
+import { CurrencyDollar, MapPin, Timer } from "@phosphor-icons/react";
+import { Header } from "../../components/Header";
+
+export function Delivery() {
+  return (
+    <>
+      <Header inCart />
+      <main className="flex flex-col items-center lg:items-start gap-10 lg:bg-delivery bg-no-repeat bg-right-bottom py-3 sm:pt-20 sm:pb-0">
+        <div className="flex flex-col gap-1">
+          <h1 className="text-yellow-dark font-title text-2xl font-extrabold text-center lg:text-left">Uhu! Pedido confirmado</h1>
+          <p className="text-brown-200 text-lg text-center lg:text-left">Agora é só aguardar que logo o café chegará até você</p>
+        </div>
+        <div className="w-full sm:w-3/4 max-w-[32.875rem] bg-gradient-to-r from-yellow from-[2.61%] to-purple to-[98.76%] p-px rounded-tl-md rounded-tr-4xl rounded-bl-4xl rounded-br-md">
+          <ul className="flex flex-col gap-8 bg-gray-50 w-full h-full p-4 sm:p-10 rounded-tl-md rounded-tr-4xl rounded-bl-4xl rounded-br-md">
+            <li className="flex items-start sm:items-center gap-3">
+              <span className="bg-purple text-gray-50 p-2 rounded-full">
+                <MapPin size={32} weight="fill" />
+              </span>
+              <p>
+                Entrega em <b>Av. dos Andradas, 3000</b>
+                <br/>Santa Efigênia - Belo Horizonte, MG
+              </p>
+            </li>
+            <li className="flex items-start sm:items-center gap-3">
+              <span className="bg-yellow text-gray-50 p-2 rounded-full">
+                <Timer size={32} weight="fill" />
+              </span>
+              <p>
+                Previsão de entrega
+                <br/><b>20 min - 30 min</b>
+              </p>
+            </li>
+            <li className="flex items-start sm:items-center gap-3">
+              <span className="bg-yellow-dark text-gray-50 p-2 rounded-full">
+                <CurrencyDollar size={32} weight="fill" />
+              </span>
+              <p>
+                Pagamento na entrega
+                <br/><b>Cartão de Crédito</b>
+              </p>
+            </li>
+          </ul>
+        </div>
+      </main>
+    </>
+  )
+}
+
+
+
+
+
