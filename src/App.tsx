d946ba18de17cec42bcc9d6188dc45dd9fa7fcd7
Bruno Vinícius <vbruno96@gmail.com>
2023-05-23 17:11:11 -0300

    create ShoppContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import { Outlet } from 'react-router-dom'
 import './global.css'
+import { ShoppContextProvider } from './contexts/ShoppContext'
 
 
 export function App() {
 
   return (
-    <div className='min-h-screen bg-gray-50 antialiased'>
-      <div className='w-full max-w-screen-2xl px-3 xl:mx-auto xl:px-40'>
-        <Outlet />
+    <ShoppContextProvider>
+      <div className='min-h-screen bg-gray-50 antialiased'>
+        <div className='w-full max-w-screen-2xl px-3 xl:mx-auto xl:px-40'>
+          <Outlet />
+        </div>
       </div>
-    </div>
+    </ShoppContextProvider>
   )
 }
---
+++ src/contexts/ShoppContext.tsx
@@ -0,0 +1,89 @@
+import { ReactNode, createContext, useCallback, useEffect, useState } from 'react'
+
+type Props = { children: ReactNode }
+
+interface CartItem {
+  id: number
+  quantity: number
+  amount: number
+  name: string
+  img: string
+}
+
+type ShoppContextData = {
+  cartItems: CartItem[]
+  removeItemCart: (coffeeId: number) => void
+  addItemCart: (cartItem: CartItem) => void
+  totalItems: number
+  totalAmount: number
+  decrementItemCart: (id: number) => void
+  incrementItemCart: (id: number) => void
+}
+
+export const ShoppContext = createContext({} as ShoppContextData)
+
+export function ShoppContextProvider ({children}: Props) {
+  const [totalItems, setTotalItems] = useState(0)
+  const [totalAmount, setTotalAmount] = useState(0)
+  const [cartItems, setCartItems] = useState<CartItem[]>([])
+
+  const addItemCart = useCallback((cartItem: CartItem) => {
+    setCartItems(prevState => [...prevState.filter(item => item.id !== cartItem.id), cartItem])
+  }, [])
+
+  const removeItemCart = useCallback((coffeeId: number) => {
+    const cartItemsWithoutSelectedItem = cartItems.filter(item => item.id !== coffeeId)
+    setCartItems(cartItemsWithoutSelectedItem)
+  }, [cartItems])
+
+  const decrementItemCart = useCallback((id: number) => {
+    setCartItems(prevState => prevState.map(item => {
+      if (item.id === id && item.quantity > 1) {
+        item.quantity -= 1
+      }
+
+      return item
+    }))
+  }, [])
+
+  const incrementItemCart = useCallback((id: number) => {
+    setCartItems(prevState => prevState.map(item => {
+      if (item.id === id) {
+        console.log(item.quantity)
+        item.quantity += 1
+      }
+
+      return item
+    }))
+  }, [])
+
+
+
+  useEffect(() => {
+    const cart = cartItems.reduce((acc, cartItem) => {
+      acc.quantityItems += cartItem.quantity
+      acc.totalAmount += cartItem.quantity * cartItem.amount 
+      return acc
+    },
+    {
+      quantityItems: 0,
+      totalAmount: 0
+    })
+    setTotalItems(cart.quantityItems)
+    setTotalAmount(cart.totalAmount)
+  }, [cartItems, setTotalItems, setTotalAmount])
+  
+  return (
+    <ShoppContext.Provider value={{
+        totalAmount,
+        totalItems,
+        removeItemCart,
+        addItemCart,
+        cartItems,
+        decrementItemCart,
+        incrementItemCart
+      }}>
+      { children }
+    </ShoppContext.Provider>
+  )
+}
