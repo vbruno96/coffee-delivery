60d49bf848633b3d30bd1c39a81bdf412d591c36
Bruno Vinícius <vbruno96@gmail.com>
2023-05-23 17:36:48 -0300

    add label on cart icon

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,10 +1,19 @@
+import { useContext } from 'react'
 import { MapPin, ShoppingCartSimple } from '@phosphor-icons/react'
+
 import Logo from '../../assets/img/logo.svg'
 
+
+import { ShoppContext } from '../../contexts/ShoppContext'
+import { Link } from 'react-router-dom'
+
 export function Navbar() {
+  const { totalItems } = useContext(ShoppContext)
   return (
     <nav className='flex justify-between items-center py-8'>
-      <img src={Logo} alt="Coffee Delivery Logo" />
+      <Link to='/'>
+        <img src={Logo} alt="Coffee Delivery Logo" />
+      </Link>
       <div className="flex items-center gap-3">
         <button
           className='flex items-center gap-1 bg-purple-light text-purple-dark p-2 rounded-md text-2sm'
@@ -16,15 +25,21 @@ export function Navbar() {
           />
           Belo Horizonte
         </button>
-        <button
-          className="relative flex justify-center items-center bg-yellow-light p-2 rounded-md after:absolute after:-top-2 after:-right-2 after:flex after:justify-center after:items-center after:content-['13'] after:w-5 after:h-5 after:bg-yellow-dark after:rounded-full after:text-white after:text-sm after:leading-none after:font-bold"
+        <Link
+          to='/checkout'
+          className={`relative flex justify-center items-center bg-yellow-light p-2 rounded-md `}
         >
+          {
+            totalItems > 0 && <span className='absolute -top-2 -right-2 w-5 h-5 flex justify-center items-center bg-yellow-dark rounded-full text-white text-sm leading-none font-bold'>
+              {totalItems > 9 ? '+9' : totalItems}
+            </span>
+          }
           <ShoppingCartSimple
             size={22}
             weight="fill"
             className='text-yellow-dark'
           />
-        </button>
+        </Link>
       </div>
     </nav>
   )
