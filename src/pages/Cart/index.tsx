8a0538cc53a35fba6a04b35a1e23158741891b6d
Bruno Vinícius <vbruno96@gmail.com>
2023-05-23 22:15:00 -0300

    add lib react-hook-form

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -1,18 +1,45 @@
+import { useNavigate } from "react-router-dom";
+import z from "zod"
+import { useForm } from 'react-hook-form'
+import { zodResolver } from "@hookform/resolvers/zod"
 import { Bank, CreditCard, CurrencyDollar, MapPinLine, Money } from "@phosphor-icons/react";
 import { Header } from "../../components/Header";
 import { formatAmount } from "../../util/formatAmount";
 import { CoffeeCart } from "../../components/CoffeeCart";
 import { useShoppContext } from "../../hooks/useShoppContext";
 
+const AddressFormSchema = z.object({
+  cep: z.string(),
+  street: z.string(),
+  number: z.string(),
+  district: z.string(),
+  neiborhood: z.string(),
+  city: z.string(),
+  state: z.string(),
+})
+
+type AddreddFormData = z.infer<typeof AddressFormSchema>
+
 export function Cart() {
   const { cartItems, totalAmount } = useShoppContext()
+  const { register, handleSubmit, formState: { errors } } = useForm<AddreddFormData>({
+    resolver: zodResolver(AddressFormSchema)
+  })
+  const navigate = useNavigate()
+
+  function handleFinishOrder(data: AddreddFormData) {
+    console.log(data)
+    //navigate('/finish')
+  }
+
+
   return (
     <>
       <Header inCart />
       <div className="flex flex-col xl:flex-row gap-8 py-10">
         <main className="xl:max-w-screen-sm flex flex-col gap-4">
           <h2 className="text-brown-200 font-title text-md font-bold">Complete seu pedido</h2>
-          <form className="flex flex-col gap-3">
+          <form id="addressForm" onSubmit={handleSubmit(handleFinishOrder)} className="flex flex-col gap-3">
             <div className="flex flex-col gap-8 bg-gray-100 p-3 sm:p-10 rounded-md">
               <div className="relative pl-[1.875rem]">
               <label className="flex items-center gap-2 text-brown-200">
@@ -29,22 +56,29 @@ export function Cart() {
                   type="text"
                   className="col-span-2 bg-gray-200 text-brown-100 placeholder:text-brown-50 p-3 rounded text-2sm uppercase ring-1 ring-gray-300 focus:ring-yellow-dark focus:outline-none"
                   placeholder="CEP"
+                  required
+                  {...register('cep')}
                 />
                 <input
                   type="text"
                   className="col-span-6 bg-gray-200 text-brown-50 placeholder:text-brown-50 p-3 rounded text-2sm uppercase ring-1 ring-gray-300 focus:ring-yellow-dark focus:outline-none"
                   placeholder="Rua"
+                  required
+                  {...register('street')}
                 />
                 <input
                   type="text"
                   className="col-span-2 bg-gray-200 text-brown-50 placeholder:text-brown-50 p-3 rounded text-2sm uppercase ring-1 ring-gray-300 focus:ring-yellow-dark focus:outline-none"
                   placeholder="Número"
+                  required
+                  {...register('number')}
                 />
                 <div className="relative col-span-4 after:absolute after:top-3.5 after:right-3 after:content-['Opcional'] after:text-brown-50 after:text-sm after:italic">
                   <input
                     type="text"
                     className="w-full bg-gray-200 text-brown-50 placeholder:text-brown-50 p-3 rounded text-2sm uppercase ring-1 ring-gray-300 focus:ring-yellow-dark focus:outline-none"
                     placeholder="Complemento"
+                    {...register('district')}
                   />
                 </div>
                 
@@ -52,16 +86,22 @@ export function Cart() {
                   type="text"
                   className="col-span-2 bg-gray-200 text-brown-50 placeholder:text-brown-50 p-3 rounded text-2sm uppercase ring-1 ring-gray-300 focus:ring-yellow-dark focus:outline-none"
                   placeholder="Bairro"
+                  required
+                  {...register('neiborhood')}
                 />
                 <input
                   type="text"
                   className="col-span-3 bg-gray-200 text-brown-50 placeholder:text-brown-50 p-3 rounded text-2sm uppercase ring-1 ring-gray-300 focus:ring-yellow-dark focus:outline-none"
                   placeholder="Cidade"
+                  required
+                  {...register('city')}
                 />
                 <input
                   type="text"
                   className="col-span-1 bg-gray-200 text-brown-50 placeholder:text-brown-50 p-3 rounded text-2sm uppercase ring-1 ring-gray-300 focus:ring-yellow-dark focus:outline-none"
                   placeholder="UF"
+                  required
+                  {...register('state')}
                 />
               </div>
             </div>
@@ -78,6 +118,7 @@ export function Cart() {
               </div>
               <div className="flex flex-col md:flex-row gap-3">
                 <button
+                  type="button"
                   className="md:flex-1 flex justify-center md:justify-start items-center gap-3 bg-gray-300 hover:bg-gray-400 text-brown-100 p-4 rounded-md text-sb uppercase focus:bg-purple-light focus:ring-1 focus:ring-purple focus:outline-none hover:transition hover:ease-in hover:duration-300"
                 >
                   <CreditCard
@@ -87,6 +128,7 @@ export function Cart() {
                   Cartão de Crédito
                 </button>
                 <button
+                  type="button"
                   className="md:flex-1 flex justify-center md:justify-start items-center gap-3 bg-gray-300 hover:bg-gray-400 text-brown-100 p-4 rounded-md text-sb uppercase focus:bg-purple-light focus:ring-1 focus:ring-purple focus:outline-none hover:transition hover:ease-in hover:duration-300"
                 >
                   <Bank
@@ -137,9 +179,11 @@ export function Cart() {
                 <span>{formatAmount(totalAmount + 3.5)}</span>
               </div>
             </div>
-            <button className="w-full bg-yellow hover:bg-yellow-dark text-white px-2 py-3 rounded-md font-bold text-lb uppercase">
-                Confirmar Pedido
-              </button>
+            <button
+              form="addressForm" 
+              className="w-full bg-yellow hover:bg-yellow-dark text-white px-2 py-3 rounded-md font-bold text-lb uppercase">
+              Confirmar Pedido
+            </button>
           </div>
         </aside>
       </div>
